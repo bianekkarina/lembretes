995c5866831d37bcb9d697303fdbcbaa56e30977
adrianelouise <hairadrianelouise@gmail.com>
2024-06-23 22:50:40 -0300

    implementação da função editar

--- lembretes.js
+++ lembretes.js
@@ -44,11 +44,43 @@ rl.question('Escolha uma opção: ', (opcao) => {
 }
 
 function adicionarLembrete() {
-	rl.question('Digite o nome do lembrete: ', (nome) => {
+	rl.question('Digite o lembrete: ', (nome) => {
 		rl.question('Digite a data de término: ', (data) => {
-				funcionarios.push({ nome: nome, data: data})
+				lembretes.push({ nome: nome, data: data})
 				console.log('lembrete adicionado com sucesso!')
 				exibirMenu()
 			})
 		})
+}
+
+
+function editarLembrete(){
+	if (lembretes.length == 0){
+		console.log("Não há lembretes adicionados")
+	} else {
+		console.log('Lista lembretes:')
+		lembretes.forEach((lembrete, index) => {
+			console.log(`${index + 1}, ${lembrete.nome}`)
+		})
+		rl.question('Digite o número do lembrete que deseja editar:', (numero) =>{
+			if(numero > 0 && numero <= lembretes.length){
+				rl.question('Digite o novo lembrete:', (nome) => {
+					rl.question('Digite o novo prazo:', (data) => {
+						rl.question('Concluido:', (concluido) => {
+							lembretes[numero - 1] = {
+								nome,
+								data,
+								concluido
+							}
+							console.log('Editado com sucesso!')
+							exibirMenu()
+						})
+					})
+				})
+			} else {
+				console.log('Número inválido, tente novamente.')
+				exibirMenu()
+			}
+		})
+	}
 }
